418f7637a58a9e692fac464d2fe0d4f074948be8
ichsansurya <ichsansuryad@gmail.com>
2022-05-17 00:57:25 +0700

    buat router

--- miniproject/src/router/index.js
+++ miniproject/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Makanan from "../views/Makanan.vue";
+import MakananDetail from "../views/MakananDetail.vue";
+import Keranjang from "../views/Keranjang.vue";
+import InvoiceMakanan from "../views/InvoiceMakanan.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +22,26 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/makanan",
+    name: "Makanan",
+    component: Makanan,
+  },
+  {
+    path: "/makanan/:id",
+    name: "MakananDetail",
+    component: MakananDetail,
+  },
+  {
+    path: "/keranjang",
+    name: "Keranjang",
+    component: Keranjang,
+  },
+  {
+    path: "/invoice-makanan",
+    name: "invoicemakanan",
+    component: InvoiceMakanan,
   },
 ];
 
